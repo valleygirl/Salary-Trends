98b919b3d728c5badb5916e13dd3ad9ae50434ed
sindhu <sindhu.r050985@gmail.com>
2016-05-04 20:41:03 -0700

    Adding RF for 17k i/p records

--- Random Forest/RandomForest/randomForest1000.R
+++ Random Forest/RandomForest/randomForest1000.R
@@ -1,30 +1,26 @@
-lca <- read.csv("RandomForest/RandomForestTraining_data.csv", header = T)
-lca<-lca[lca["LCA_CASE_WAGE_RATE_UNIT"]=="Year",]
+randomForest1000.csv <- read.csv("RandomForest/RanForest1000.csv", header = T)
 
-attach(lca)
-str(lca)
-names(lca)
+attach(randomForest1000.csv)
 
-LCA_CASE_EMPLOYER_NAME <- lca[, 'LCA_CASE_EMPLOYER_NAME']
-LCA_CASE_EMPLOYER_CITY <- lca[, 'LCA_CASE_EMPLOYER_CITY']
-LCA_CASE_JOB_TITLE <- lca[, 'LCA_CASE_JOB_TITLE']
-SALARY <- as.numeric(lca[, 'LCA_CASE_WAGE_RATE_FROM'])
-basedata <- data.frame(LCA_CASE_EMPLOYER_NAME, LCA_CASE_EMPLOYER_CITY,
-                       LCA_CASE_JOB_TITLE, SALARY)
-basedata[c("SALARY_RANGE")] <- ""
+str(randomForest1000.csv)
 
-basedata$SALARY_RANGE[basedata$SALARY<50000] <- "Below 50000"
-basedata$SALARY_RANGE[basedata$SALARY>=50000 & basedata$SALARY<75000] <- "50000 to 75000"
-basedata$SALARY_RANGE[basedata$SALARY>=75000 & basedata$SALARY<100000] <- "75000 to 100000"
-basedata$SALARY_RANGE[basedata$SALARY>=100000 & basedata$SALARY<125000] <- "100000 to 125000"
-basedata$SALARY_RANGE[basedata$SALARY>=125000 & basedata$SALARY<150000] <- "125000 to 150000"
-basedata$SALARY_RANGE[basedata$SALARY>=150000 & basedata$SALARY<200000] <- "150000 to 200000"
-basedata$SALARY_RANGE[basedata$SALARY>200000] <- "Above 200000"
+names(randomForest1000.csv)
 
-LCA_CASE_EMPLOYER_NAME <- basedata[, 'LCA_CASE_EMPLOYER_NAME']
-LCA_CASE_EMPLOYER_CITY <- basedata[, 'LCA_CASE_EMPLOYER_CITY']
-LCA_CASE_JOB_TITLE <- basedata[, 'LCA_CASE_JOB_TITLE']
-SALARY_RANGE <- basedata[, 'SALARY_RANGE']
+#randomForest1000.csv[, 'LCA_CASE_EMPLOYER_NAME'] <- as.numeric(randomForest1000.csv[, 'LCA_CASE_EMPLOYER_NAME'])
+
+#randomForest1000.csv[, 'LCA_CASE_EMPLOYER_CITY'] <- as.numeric(randomForest1000.csv[, 'LCA_CASE_EMPLOYER_CITY'])
+
+#randomForest1000.csv[, 'LCA_CASE_EMPLOYER_POSTAL_CODE'] <- as.numeric(randomForest1000.csv[, 'LCA_CASE_EMPLOYER_POSTAL_CODE'])
+
+#randomForest1000.csv[, 'LCA_CASE_JOB_TITLE'] <- as.numeric(randomForest1000.csv[, 'LCA_CASE_JOB_TITLE'])
+
+#randomForest1000.csv[, 'CODE'] <- as.numeric(randomForest1000.csv[, 'CODE'])
+
+# my changes below
+LCA_CASE_EMPLOYER_NAME <- randomForest1000.csv[, 'LCA_CASE_EMPLOYER_NAME']
+LCA_CASE_EMPLOYER_CITY <- randomForest1000.csv[, 'LCA_CASE_EMPLOYER_CITY']
+LCA_CASE_JOB_TITLE <- randomForest1000.csv[, 'LCA_CASE_JOB_TITLE']
+SALARY_RANGE <- randomForest1000.csv[, 'SALARY_RANGE']
 prediction <- data.frame(LCA_CASE_EMPLOYER_NAME, LCA_CASE_EMPLOYER_CITY,
                          LCA_CASE_JOB_TITLE, SALARY_RANGE)
 
@@ -33,7 +29,7 @@ prediction <- data.frame(LCA_CASE_EMPLOYER_NAME, LCA_CASE_EMPLOYER_CITY,
 
 #install.packages("ggplot2")
 
-#install.packages("randomForest")
+install.packages("randomForest")
 
 #install.packages("e1071")
 
@@ -47,10 +43,13 @@ library(e1071)
 
 # my changes
 inTrain <- createDataPartition(y = prediction$SALARY_RANGE, p = .7, list = FALSE)
+#inTrain <- createDataPartition(y = randomForest1000.csv$SALARY_RANGE, p = .7, list = FALSE)
 
 training <- prediction[inTrain,]
+#training <- randomForest1000.csv[inTrain,]
 
 testing <- prediction[ - inTrain,]
+#testing <- randomForest1000.csv[ - inTrain,]
 
 ranForestModel <- train(SALARY_RANGE ~ ., data = training, method = 'rf', prox = TRUE)
 
@@ -58,15 +57,19 @@ ranForestModel
 
 getTree(ranForestModel$finalModel, k = 2)
 
-LCA_CASE_EMPLOYER_NAME <- "A2Z DEVELOPMENT CENTER, INC."
-LCA_CASE_EMPLOYER_CITY <- "SAN JOSE"
-LCA_CASE_JOB_TITLE <- "QUALITY ASSURANCE ENGINEER II"
-pred <- predict(ranForestModel, data.frame(
-  LCA_CASE_EMPLOYER_NAME,
-  LCA_CASE_EMPLOYER_CITY,
-  LCA_CASE_JOB_TITLE))
+#predicting the Testing set
+
+pred <- predict(ranForestModel, testing)
 
 pred
 
-save(ranForestModel,file = "ranForestModel.RData")
+testing$predRight <- pred == testing$SALARY_RANGE
+
+#table(pred, testing$SALARY_RANGE)
+
+#qplot(LCA_CASE_EMPLOYER_CITY,LCA_CASE_JOB_TITLE, colour=predRight,data=testing,main='Predictions')
+
+#qplot(LCA_CASE_EMPLOYER_CITY,LCA_CASE_EMPLOYER_NAME, colour=predRight,data=testing,main='Predictions')
+
+#qplot(LCA_CASE_EMPLOYER_NAME,LCA_CASE_JOB_TITLE, colour=predRight,data=testing,main='Predictions')
 
